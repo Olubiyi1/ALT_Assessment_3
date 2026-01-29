484cba6d2453b5f312da8609aa6af8c45e8c83e7
olubiyi babajide <olubiyibabajide@gmail.com>
2026-01-30 00:56:30 +0100

    route

--- src/User/user.controller.js
+++ src/User/user.controller.js
@@ -10,7 +10,7 @@ class UserController {
       // If request came from EJS form (not JSON), set session and redirect
       if (req.headers.accept?.includes("text/html")) {
         req.session.user = { id: user.id, email: user.email };
-        return res.redirect("/api/tasks/task-Page");
+        return res.redirect("/api/tasks/tasks-Page");
       }
 
       // Else, API response
@@ -33,7 +33,7 @@ class UserController {
         };
       // If request came from EJS form
       if (req.headers.accept?.includes("text/html")) {
-        return res.redirect("api/tasks/tasks-page");
+        return res.redirect("/api/tasks/tasks-page");
       }
 
       // Else, API response
